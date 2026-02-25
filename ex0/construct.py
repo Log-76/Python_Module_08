0bca55d0e70dc8e288fcd4cd95264e86f35e5dfa
Logann Leriche <lleriche@c1r6p3.42lehavre.fr>
2026-02-25 09:24:18 +0100

    module08

--- ex0/construct.py
+++ ex0/construct.py
@@ -1,9 +1,28 @@
 import sys
-import os
 import site
 
 
 try:
-    print("MATRIX STATUS: You're still plugged in")
+    if sys.prefix != sys.base_prefix:
+        print("MATRIX STATUS: You're still plugged in\n")
+        print(f"Current Python: {sys.executable}")
+        print(f"Environment Path: {sys.prefix}\n")
+        print("SUCCESS: You're in an isolated environment! Safe to install"
+              "packages without affecting the global system.\n")
+        print("Package installation path:")
+        print(site.getsitepackages()[0])
+    else:
+        raise Exception
 except Exception:
-    print(1)
+    print("MATRIX STATUS: You're still plugged in\n")
+    print(f"Current Python: {sys.executable}")
+    print("Virtual Environment: None detected\n")
+    print("WARNING: You're in the global environment! The machines can see"
+          "everything you install.\n")
+    print("To enter the construct, run:")
+    print("python -m venv matrix_env")
+    print("source matrix_env/bin/activate # On Unix")
+    print("matrix_env")
+    print("Scripts")
+    print("activate # On Windows\n")
+    print("Then run this program again.")
